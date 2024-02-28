9726c344fe732fa3a8546d59c906c3f7a72d0c57
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-28 16:27:02 +0530

    Create while_p.py

---
+++ while_p.py
@@ -0,0 +1,12 @@
+var=7
+while var>0:
+    print("current varuiable value :",var)
+    var= var-1
+    if var==3:
+        break
+    else:
+        if var==6:
+            var=var-1
+            continue
+    print("good bye!")
+print("happy coding bro")
