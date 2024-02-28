7d32e0e66845f8853272534f43dc6993811f7986
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-28 17:21:43 +0530

    Create leap_p.py

---
+++ leap_p.py
@@ -0,0 +1,6 @@
+#program to find the leap year
+year =int(input("enter the year:"))
+if (year %4==0):
+    print("the year",year,"is leap year")
+else:
+    print("the year ",year ,"is not leap year")
