171169477504f5c08c1dd210ce57fbe3d6908fe5
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-28 16:35:13 +0530

    Create apply_to_licence.py

---
+++ apply_to_licence.py
@@ -0,0 +1,10 @@
+#program to check the eleigibility for driving licencc
+name=input("what is your name")
+print(name)
+age =int(input("whai is your age?"))
+print(age)
+if age >=18:
+    print("your elegible to apply for the driving licence")
+else:
+    print("your not elegible to apply for  the driving licence")
+    
